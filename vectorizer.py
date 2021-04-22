37bf0a9f4668c951ecc9466a10248305cd294168
JoeBorn <joeborn@gmail.com>
2021-04-22 09:10:44 -0500

    TP0 submission

--- vectorizer.py
+++ vectorizer.py
@@ -10,7 +10,7 @@ import decimal
 
 #TODO: grid screws up when canvas stretched
 #file = 'C:/GitHub/VectNN/JB_4.bmp'
-file = 'C:/mnist/mnist_all_files/training/7/10394.png'  
+file = 'C:/mnist/mnist_all_files/training/4/9.png'  
 img = Image.open(file)
 #img.show()
 def appStarted(app):
